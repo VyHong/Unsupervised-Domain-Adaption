ff6a1085c65223972deb1701f6cdb4aadd2b16b3
vyhong <vy.hong07@gmail.com>
2025-12-04 14:24:30 +0100

    fix sampling

--- src/utils/image_manipulation.py
+++ src/utils/image_manipulation.py
@@ -9,7 +9,7 @@ def resample_logits(tensor, target_size):
     resampled_logits = F.interpolate(
         tensor,
         size=target_size,
-        mode="bilinear",
+        mode="nearest",
         align_corners=False,  # Always set to False for segmentation upsampling
     )
     return resampled_logits
